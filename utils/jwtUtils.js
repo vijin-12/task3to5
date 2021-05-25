c1f36bef44cb6edb5539e43dd8b9cbc8603f4d70
vijin-12 <justinselin1982@gmail.com>
2021-05-25 20:45:50 +0530

    clering unwanted codes

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -34,20 +34,3 @@ exports.createSendToken = function (user, statusCode, req, res){
     });
 };
 
-
-exports.createSendTokenAndRedirect = function (user, statusCode, req, res){
-    const token = signToken_(user);
-  
-    res.cookie('jwt', token, {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-    });
-  
-    // Remove password from output
-    user.password = undefined;
-  
-    res.status(statusCode).redirect('/accountActivated')
-};
